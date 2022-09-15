a2775dc364b2f495fb5345ed9c27ec2eeb201d5d
theMorshed <themorshedctg@gmail.com>
2022-09-15 11:11:26 +0600

    final polishing and fix some responsive issue

--- script.js
+++ script.js
@@ -113,7 +113,7 @@ const start = () => {
 
   const startCountdown = setInterval(() => {
     countdownOverlay.innerHTML = `<h1>${count}</h1>`;
-
+    
     // finished timer
     if (count === 0) {
       // -------------- START TYPING -----------------
